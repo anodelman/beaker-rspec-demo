6228e588b4b82a292e814ec5af8d5829d2bfe78e
Alice Nodelman <alice@puppetlabs.com>
2015-01-06 15:26:18 -0800

    add documentation links

--- user-demo/spec/acceptance/demo_spec.rb
+++ user-demo/spec/acceptance/demo_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper_acceptance'
 describe "my tests" do
 
   # an example using the beaker DSL
+  # use http://www.rubydoc.info/github/puppetlabs/beaker/Beaker/DSL
   it "should say hello!" do
     hosts.each do |host|
       on host, 'echo hello' do |result|
@@ -12,6 +13,7 @@ describe "my tests" do
   end
 
   # an example using Serverspec
+  # use http://serverspec.org/resource_types.html
   describe package('puppet') do
     it { should be_installed }
   end
